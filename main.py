51b168926b829f4934e65aa07b61f487f202c218
CERM <carlos.rosas7476@alumnos.udg.mx>
2023-09-23 16:26:30 -0600

    Comentarios

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ import sys
 
 
 class Lexico(object):
+    #Constructor de la clase
     def __init__(self, cadena, id, tipo):
         self.cadena = cadena
         self.token = id
         self.tipo = tipo
-
+    #Retorna la cadena y el token
     def __str__(self):
         return "%s  %s" % (self.cadena, self.token)
-        
+    #Retorna la cadena
     def obtenercadena(self):
        return self.cadena
-
+    #Retorna el token
     def obtenertoken(self):
         return self.token
-
+    #Retorna la descripcion
     def obtenerdescripcion(self):
         return self.tipo
 
     
 class MainWindow(QMainWindow):
-    
+    #
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setFixedSize(980,650)
@@ -74,17 +75,21 @@ class MainWindow(QMainWindow):
         size = len(cadena)
         cadena = cadena + " "
         cadenaTemp = ""
-
+        # Ciclo que recorre la cadena
         while i < size:
             cadenaTemp = ""
+            # Si es una letra
             if cadena[i].isalpha() == True:
+                # Ciclo que recorre la cadena
                 while cadena[i].isalpha() == True or cadena[i].isdigit() == True:
+                    # Si es una letra
                     if cadena[i] == " ":
                         break
                     cadenaTemp = cadenaTemp + cadena[i]
                     i = i+1
                     if i==size:
                         break
+                # Si la cadena es una palabra reservada
                 if cadenaTemp == "if":
                     tiposDeDato = Lexico(cadenaTemp, 9, "IF")
                     listaLexico.append(tiposDeDato)
@@ -112,7 +117,7 @@ class MainWindow(QMainWindow):
                 else:
                     tiposDeDato = Lexico(cadenaTemp, 1, "ID")
                     listaLexico.append(tiposDeDato)
-
+            # Si es un digito
             elif cadena[i].isdigit() == True:
                 while cadena[i].isdigit() == True:
                     cadenaTemp = cadenaTemp + cadena[i]
@@ -121,89 +126,89 @@ class MainWindow(QMainWindow):
                         break
                 tiposDeDato = Lexico(cadenaTemp, 13, "Constante")
                 listaLexico.append(tiposDeDato)
-                
+            # Si es un caracter de tipo operarador logico    
             elif (cadena[i] == "|" and cadena[i+1] == "|") or (cadena[i] == "&" and cadena[i+1] == "&"):
                cadenaTemp = cadena[i] + cadena[i+1]
-               tiposDeDato = Lexico(cadenaTemp, 17, "Operador logico")
+               tiposDeDato = Lexico(cadenaTemp, 15, "Operador logico")
                listaLexico.append(tiposDeDato)
                i = i +2
-            
+            # Si es un caracter de tipo operador suma
             elif cadena[i] == "+" or cadena[i] == "-":
                 cadenaTemp = cadena[i]
                 tiposDeDato = Lexico(cadenaTemp, 14, "Operador suma")
                 listaLexico.append(tiposDeDato)
                 i = i +1
             
-            
+            # Si es un caracter de tipo operador multiplicacion
             elif cadena[i] == "*" or cadena[i] == "/":
                 cadenaTemp = cadena[i]
                 tiposDeDato = Lexico(cadenaTemp,16, "Operador multiplicación ")
                 listaLexico.append(tiposDeDato)
                 i = i +1
-
+            # Si es un caracter de tipo operador relacional
             elif (cadena[i] == "=" and cadena[i+1] == "=") or (cadena[i] == "<" and cadena[i+1] == "=") or (cadena[i] == ">" and cadena[i+1] == "=") or (cadena[i] == "!" and cadena[i+1] == "="):
                 cadenaTemp = cadena[i] + cadena[i+1]
                 tiposDeDato = Lexico(cadenaTemp, 17, "Operador relacional")
                 listaLexico.append(tiposDeDato)
                 i = i +2
-
+            # Si es un caracter de tipo operador relacional
             elif cadena[i] == "<" or cadena[i] == ">":
                 cadenaTemp = cadena[i]
                 tiposDeDato = Lexico(cadenaTemp, 17, "Operador relacional ")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-
+            # Sie es un $
             elif cadena[i] == "$":
                 cadenaTemp = "$"
                 tiposDeDato = Lexico(cadenaTemp, 18, "$")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es un ;
             elif cadena[i] == ";":
                 cadenaTemp = ";"
                 tiposDeDato = Lexico(cadenaTemp, 2, "Punto y coma")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es una ,
             elif cadena[i] == ",":
                 cadenaTemp = ","
                 tiposDeDato = Lexico(cadenaTemp, 3, "Coma")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es un (
             elif cadena[i] == "(":
                 cadenaTemp = "("
                 tiposDeDato = Lexico(cadenaTemp, 4, "Parentesis izquierdo")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es un )
             elif cadena[i] == ")":
                 cadenaTemp = ")"
                 tiposDeDato = Lexico(cadenaTemp, 5, "Parentesis derecho")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es un {
             elif cadena[i] == "{":
                 cadenaTemp = "{"
                 tiposDeDato = Lexico(cadenaTemp, 6, "Llave izquierda")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-
+            # Si es un }
             elif cadena[i] == "}":
                 cadenaTemp = "}"
                 tiposDeDato = Lexico(cadenaTemp, 7, "Llave derecha")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-            
+            # Si es un =
             elif cadena[i] == "=":
                 cadenaTemp = "="
                 tiposDeDato = Lexico(cadenaTemp, 8, "Igual")
                 listaLexico.append(tiposDeDato)
                 i = i + 1
-
+            # Si es un espacio
             elif cadena[i] == " ":
                 i = i + 1
-
+            # Si es un caracter invalido
             else:
                 i = i+1
 
